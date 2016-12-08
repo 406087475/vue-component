2942e1e93487baea3bc5dbe452c242d16f2ba26a
jiancheng01.li <406087475@qq.com>
2016-12-08 17:48:00 +0800

    调整 eslint 规则，括号里可以有空格

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     // allow paren-less arrow functions
     'arrow-parens': 0,
     // allow async-await
+    'space-in-parens': 0,
     'generator-star-spacing': 0,
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
